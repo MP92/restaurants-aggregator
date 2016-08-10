811d87945e1432059c911e76e72236fd50106242
MP92 <max.nn92@gmail.com>
2016-08-10 15:48:46 +0300

    Add CLEARDB credentials for heroku and sql script

--- config/dbParams.php
+++ config/dbParams.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-    "host" => "localhost",
-    "dbName" => "restaurants_db",
-    "login" => "root",
-    "password" => "",
+    "host" => "us-cdbr-iron-east-04.cleardb.net",
+    "dbName" => "heroku_fdd0efd30762729",
+    "login" => "b89acd9d44c81b",
+    "password" => "ddb73fa4",
 );
